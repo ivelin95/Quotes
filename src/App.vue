b458382f0a8c7fead85dc1fa144ecfde0e741563
ivelin95 <61183498+ivelin95@users.noreply.github.com>
2021-01-27 00:11:58 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quotes @quoteAdd="NewQ"></app-new-quotes>
+    <app-quoetes
+      v-bind:quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quoetes>
+    <div class="row">
+      <div class="col-sm-12 text center">
+        <div class="alert alert-info text-center" >
+          Info: Click on a Quote to delete it !
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuotesGrid from "./components/QuotesGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data: function () {
+    return {
+      maxQuotes: 10,
+      quotes: ["Test text for quote"],
+    };
+  },
+  methods: {
+    NewQ(quote) {
+
+      
+      if(this.quotes.length >= this.maxQuotes){
+         return alert("Please, Delete Quotes First")
+      }
+      if (quote != "") {
+        this.quotes.push(quote);
+      }
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    appQuoetes: QuotesGrid,
+    appNewQuotes: NewQuote,
+    appHeader: Header,
+  },
+};
+</script>
+
+<style>
+body{
+   margin-left: 5%;
+}
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row">
+    <form>
+      <div
+        class="col-sm-8 com-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+      >
+        <label>Quote</label>
+        <textarea class="form-control" rows="4" v-model="quote"></textarea>
+        
+          <button class="btn button-primary btn_pos" @click.prevent="createNew">
+            Add Quote
+          </button>
+        
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      quote: "",
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdd", this.quote);
+      this.quote = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+      .btn_pos{
+            position: relative;
+            left: 40%;
+      }
+</style>
+
